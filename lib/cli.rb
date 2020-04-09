7b2a60b05ba7fb32a1d8967af50ece756dfb7a4d
tarujkhan <60187302+tarujkhan@users.noreply.github.com>
2020-04-09 12:41:13 -0500

    Fixes syntax issues in cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,43 +1,46 @@
 class CLI 
-  def call 
+   def call 
     Scraper.scrape_thanksgiving
     puts "Welcome to the 2020 Thanksgiving Parade in New York"
     puts "What would you like to know about the Parade?"
    
     list_tabs
     menu
-end
+   end
 
-def list_tabs
-  Thanksgiving.each.with_index(1) do |tab, i|
-    puts "#{i}, #{tab}"
-end 
+  def list_tabs
+    Thanksgiving.each.with_index(1) do |tab, i|
+      puts "#{i}, #{tab}"
+    end 
+  end 
 
-def menu 
+  def menu 
    puts "Please choose from the following options:"
    input gets.chomp
-  if !input.to_i.between(1, Thanksgiving.all.count)
+    if !input.to_i.between(1, Thanksgiving.all.count)
     puts "Tab information not found. Please select a different number!"
     list_tabs
     menu 
-  else 
+    else 
     tab = Thanksgiving.all(input.to_i-1)
     display_tab_details(tab)
+    end 
   end 
   puts "Would you like to see information about another tab"
   puts "Please enter Y or N"
   another_tab = gets.strip.downcase 
   if another_tab == "y" 
     list_tabs
-  menu 
-else 
-  if another_tab == "n"
+    menu 
+  else 
+    if another_tab == "n"
     puts "I hope you have fun at the Parade"
     exit
-  else 
+    else 
     puts "I did not understand. Please try again"
     list_tabs
     menu
+    end 
   end 
 end 
 
@@ -52,6 +55,5 @@ def display_tab_details(tab)
   puts "OnlyOnNBC: #{thanksgiving.onlyonnbc}"
   puts "VerizonLive: #{thanksgiving.verizonlive}"
   puts "Faqs: #{thanksgiving.faqs}"
-  
-    
 end 
+
